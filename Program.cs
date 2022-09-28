3c656f953549d661c88dcaf33c9a5127cacf0103
jayukasode23 <jayukasode2398@gmail.com>
2022-09-28 12:15:01 +0530

    Added Greeting Messege

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using LineComparisonProblem;
+
+Console.WriteLine("Welcome To Line Comparison Computation Program");
+
+CheckLength.LineComparison();
+
+CheckEquality.CheckEqual();
+
+CompareLine.CompareLines();
